c129c2e6fdb141746b4ac0f6b7e516bdc3429c47
Daniel <danielnatsufire@gmail.com>
2022-04-13 00:18:31 -0300

    quinta versao - tela de entrada conectar e validar nome do usuario e loading gif

---
+++ script.js
@@ -0,0 +1,26 @@
+// Signin on Chat
+function signin() {
+    const nameInput = document.querySelector('.name-section input').value
+    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', { name: nameInput })
+
+    document.querySelector('.name-section').classList.add('hidden')
+    document.querySelector('.loading').classList.remove('hidden')
+
+    promise.then(getInChat)
+    promise.catch(validateName)
+}
+
+function getInChat() {
+    document.querySelector('.name-error').innerHTML = ''
+    document.querySelector('.signin').classList.add('hidden')
+
+    setInterval(keepUserConnected, 5000)
+}
+
+function validateName(err) {
+    document.querySelector('.name-section').classList.remove('hidden')
+    document.querySelector('.loading').classList.add('hidden')
+    document.querySelector('.name-error').innerHTML = `Erro ${err.response.status} - Nome já inserido no chat! Tente outro...`
+    document.querySelector('.name-section input').value = ''
+}
+
