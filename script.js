3e93277c2c86635da8dfcf84e0e2ca3b0f75c614
Daniel <danielnatsufire@gmail.com>
2022-04-14 01:22:01 -0300

    decima primeira versao - enviar mensagens reservadas e exibir com filtro, selecionar a pessoa e visibilidade e aparecer ao enviar no input

--- script.js
+++ script.js
@@ -1,117 +1,179 @@
+let userToSend;
+let messageStatus;
+console.log(userToSend)
+
 // Signin on chat
 function signin() {
-    const nameInput = document.querySelector('.name-section input').value
-    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', { name: nameInput })
+    const nameInput = document.querySelector(".name-section input").value;
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", { name: nameInput });
 
-    document.querySelector('.name-section').classList.add('hidden')
-    document.querySelector('.loading').classList.remove('hidden')
+    document.querySelector(".name-section").classList.add("hidden");
+    document.querySelector(".loading").classList.remove("hidden");
 
-    promise.then(getInChat)
-    promise.catch(validateName)
+    promise.then(getInChat);
+    promise.catch(validateName);
 }
 
 function getInChat() {
-    document.querySelector('.name-error').innerHTML = ''
-    document.querySelector('.signin').classList.add('hidden')
+    document.querySelector(".name-error").innerHTML = "";
+    document.querySelector(".signin").classList.add("hidden");
 
-    setInterval(keepUserConnected, 5000)
-    setInterval(getMessages, 3000)
-    setInterval(getAllUsersConnected, 3000)
+    setInterval(keepUserConnected, 5000);
+    setInterval(getMessages, 3000);
+    getAllUsersConnected();
 }
 
 function validateName(err) {
-    document.querySelector('.name-section').classList.remove('hidden')
-    document.querySelector('.loading').classList.add('hidden')
-    document.querySelector('.name-error').innerHTML = `Erro ${err.response.status} - Nome já inserido no chat! Tente outro...`
-    document.querySelector('.name-section input').value = ''
+    document.querySelector(".name-section").classList.remove("hidden");
+    document.querySelector(".loading").classList.add("hidden");
+    document.querySelector(".name-error").innerHTML = `Erro ${err.response.status} - Nome já inserido no chat! Tente outro...`;
+    document.querySelector(".name-section input").value = "";
 }
 
 // Validating user on server
 function keepUserConnected() {
-    const nameInput = document.querySelector('.name-section input').value
-    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', { name: nameInput })
+    const nameInput = document.querySelector(".name-section input").value;
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", { name: nameInput });
 
-    promise.then(res => res.data)
+    promise.then((res) => res.data);
 }
 
 // Get all messages
 function getMessages() {
-    const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages')
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    const nameInput = document.querySelector(".name-section input").value;
 
-    promise.then(res => {
-        document.querySelector('main').innerHTML = ''
-        document.querySelector('main').innerHTML ? document.querySelector('.loading-messages').classList.remove('hidden') : document.querySelector('.loading-messages').classList.add('hidden')
+    promise.then((res) => {
+        document.querySelector("main").innerHTML = "";
+        document.querySelector("main").innerHTML ? document.querySelector(".loading-messages").classList.remove("hidden") : document.querySelector(".loading-messages").classList.add("hidden");
+
+        let typeMessage;
 
         for (let i = 0; i < res.data.length; i++) {
-            let inOut = res.data[i].type === 'status' ? 'in-out-color' : ''
-            let reserved = res.data[i].type === 'private_message' ? 'reserved-color' : ''
-            let typeMessage = res.data[i].type ? inOut : reserved
+            if (res.data[i].type === 'status') typeMessage = 'in-out-color'
+            else if (res.data[i].type === 'message') typeMessage = ''
 
-            document.querySelector('main').innerHTML += `
-                <div class="message ${typeMessage}">
-                    <p class="text">
-                        <span class="hour">${res.data[i].time}</span>
-                        <strong>${res.data[i].from}</strong> para <strong>${res.data[i].to}</strong>: ${res.data[i].text}
-                    </p>
-                </div>
-            `
+            if (res.data[i].type === "private_message") {
+                if(res.data[i].to === nameInput || res.data[i].from === nameInput) {
+                    document.querySelector("main").innerHTML += `
+                        <div class="message reserved-color">
+                            <p class="text">
+                                <span class="hour">(${res.data[i].time})</span>
+                                <strong>${res.data[i].from}</strong> reservadamente para <strong>${res.data[i].to}</strong>: ${res.data[i].text}
+                            </p>
+                        </div>
+                    `;
+                } else document.querySelector("main").innerHTML += ''
+            } else {
+                document.querySelector("main").innerHTML += `
+                    <div class="message ${typeMessage}">
+                        <p class="text">
+                            <span class="hour">(${res.data[i].time})</span>
+                            <strong>${res.data[i].from}</strong> para <strong>${res.data[i].to}</strong>: ${res.data[i].text}
+                        </p>
+                    </div>
+                `;
+            }
         }
         // Scroll to Last Message
-        let screenHeight = '' + window.innerHeight / 8
-        document.querySelector('main').lastElementChild.scrollIntoView(false)
-        window.scrollBy(0, screenHeight)
-    })
+        let screenHeight = "" + window.innerHeight / 8;
+        document.querySelector("main").lastElementChild.scrollIntoView(false);
+        window.scrollBy(0, screenHeight);
+    });
 }
 
 // Send Message
 function sendMessage() {
-    const nameInput = document.querySelector('.name-section input').value
-    let messageInput = document.querySelector('footer input')
+    const nameInput = document.querySelector(".name-section input").value;
+    let messageInput = document.querySelector("footer input");
+
+    let to = userToSend !== undefined ? userToSend : "Todos";
+    let type;
+    if (messageStatus !== undefined) {
+        if (messageStatus === "Público") type = "message";
+        else if (messageStatus === "Reservadamente") type = "private_message";
+    } else {
+        type = "message"
+    }
 
     let messageValues = {
         from: nameInput,
-        to: 'Todos',
+        to: to,
         text: messageInput.value,
-        type: 'message'
-    }
+        type: type,
+    };
 
-    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', messageValues)
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", messageValues);
 
-    promise.then(res => res.data)
-    promise.catch(err => {
-        alert(`${err.response.status} Usuário saiu da sala`)
-        window.location.reload()
-    })
+    promise.then((res) => res.data);
+    promise.catch((err) => {
+        alert(`${err.response.status} Usuário saiu da sala`);
+        window.location.reload();
+    });
 
-    messageInput.value = ''
+    messageInput.value = "";
 }
 
 // Get All Users Connected
 function getAllUsersConnected() {
-    const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants')
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants");
 
-    promise.then(res => {
-        document.querySelector('.users-connected').innerHTML = ''
+    promise.then((res) => {
+        document.querySelector(".users-connected").innerHTML = "";
 
-        for(let i = 0; i < res.data.length; i++) {
-            document.querySelector('.users-connected').innerHTML += `
-                <div class="user" onclick="selectUser()">
+        for (let i = 0; i < res.data.length; i++) {
+            document.querySelector(".users-connected").innerHTML += `
+                <div class="user" onclick="selectUser(this)">
                     <ion-icon name="person-circle"></ion-icon>
                     <p>${res.data[i].name}</p>
-                    <img class="hidden" src="images/check.png" alt="check">
+                    <img src="images/check.png" alt="check">
                 </div>
-            `
+            `;
         }
-    })
+    });
 }
 
 // Toggle Users Connected Sidebar
 function toggleUserSidebar() {
-    document.querySelector('aside').classList.remove('hidden') 
-    document.querySelector('aside > div').classList.add('active')
+    document.querySelector("aside").classList.add('active');
+    document.querySelector(".black-screen").classList.remove('hidden');
 
-    document.querySelector('aside > div:first-child').onclick = () => {
-        document.querySelector('aside > div').classList.remove('active')
-        document.querySelector('aside').classList.add('hidden') 
+    document.querySelector(".black-screen").onclick = () => {
+        document.querySelector("aside").classList.remove('active');
+        document.querySelector(".black-screen").classList.add('hidden');
     }
+}
+
+// Select User or "Todos"
+function selectUser(user) {
+    const selected = document.querySelector(".selected");
+    let toUser = document.querySelector('.to-message h4')
+
+    if (selected !== null) {
+        selected.classList.remove("selected");
+    }
+    user.classList.add("selected");
+
+    if (user.classList.contains("selected")) {
+        userToSend = user.querySelector('p').innerHTML;
+        toUser.innerHTML = `Enviando para ${userToSend} (Público)`
+    }
+    console.log(userToSend);
+}
+
+// Select Visibility Private or Public
+function selectVisibility(visible) {
+    const status = document.querySelector(".status");
+    let toUser = document.querySelector('.to-message h4')
+
+    if (status !== null) {
+        status.classList.remove("status");
+    }
+    visible.classList.add("status");
+
+    if (visible.classList.contains("status")) {
+        messageStatus = visible.querySelector('h3').innerHTML;
+        toUser.innerHTML = `Enviando para ${userToSend} (${messageStatus})`
+    }
+    console.log(messageStatus);
 }
